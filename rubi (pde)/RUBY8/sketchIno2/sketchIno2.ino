24d3af20e7f7c36504b1a629fa5ec864eb7edc0b
nbk2938 <104725611+Nbk2938@users.noreply.github.com>
2022-08-08 09:39:58 +0200

    Update sketchIno2.ino

--- rubi (pde)/RUBY8/sketchIno2/sketchIno2.ino
+++ rubi (pde)/RUBY8/sketchIno2/sketchIno2.ino
@@ -1,5 +1,5 @@
-#include <Tic.h>     //libreria driver
-#include <FastLED.h>   //libreria led
+#include <Tic.h>     //library driver
+#include <FastLED.h>   //library led
 
 #ifdef SERIAL_PORT_HARDWARE_OPEN
 #define ticSerial SERIAL_PORT_HARDWARE_OPEN
@@ -8,14 +8,14 @@
 SoftwareSerial ticSerial();
 #endif
 
-TicSerial tic1(Serial1, 1); //nominazione dei led
+TicSerial tic1(Serial1, 1); //declare the drivers
 TicSerial tic2(Serial1, 2);
 TicSerial tic3(Serial1, 3);
 TicSerial tic4(Serial1, 4);
 TicSerial tic5(Serial1, 5);
 TicSerial tic6(Serial1, 6);
 
-#define LED_PIN1     51   //variabili per controllo led
+#define LED_PIN1     51   //variables for led control
 #define LED_PIN2     50
 #define NUM_LEDS    15
 #define BRIGHTNESS  70
@@ -30,27 +30,27 @@ extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 int pin1 = 49;
 int pin2 = 48;
 
-int whaitTillFinished = 130;   //tempo stimato per una rotazione  [in millisecondi] 120
+int whaitTillFinished = 140;   //estimated time for 90° rotation  [in milliseconds]
 int currentValue = 0;           
 String bits = "";
 
 boolean solCorrect = false; 
-boolean onlyOneTime = false;
+//boolean scramble = true;
 
 //-----------------------------------------------------------------------------------------------
 
 void setup()  {
-  Serial.begin(9600);
+  Serial.begin(9600); //begin serial comnication with motor driver and pc
   ticSerial.begin(9600);
   
-  pinMode(49,OUTPUT);  //output di 5V per ledstrip
+  pinMode(49,OUTPUT);   //output 5V for the ledstrip
   pinMode(48,OUTPUT);
   digitalWrite(49, HIGH);
   digitalWrite(48, HIGH);
     
   delay(20);
 
-  tic1.haltAndSetPosition(0);
+  tic1.haltAndSetPosition(0); //declare a zero position for the motors and hold the position
   tic2.haltAndSetPosition(0);
   tic3.haltAndSetPosition(0);
   tic4.haltAndSetPosition(0);
@@ -58,7 +58,7 @@ void setup()  {
   tic6.haltAndSetPosition(0);
   
 
-  tic1.exitSafeStart();
+  tic1.exitSafeStart();  //motors can be ordered to move
   tic2.exitSafeStart();
   tic3.exitSafeStart();
   tic4.exitSafeStart();
@@ -67,11 +67,11 @@ void setup()  {
   
   delay(880); // power-up safety delay
     
-  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip ); //declare led strips
   FastLED.addLeds<LED_TYPE, LED_PIN2, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
     
-  for(int i = 0; i < NUM_LEDS; i++){
+  for(int i = 0; i < NUM_LEDS; i++){ //set color for ledstrip
     leds[i] = CRGB:: White; 
   }
 } 
@@ -79,19 +79,15 @@ void setup()  {
 //-----------------------------------------------------------------------------------------------
 
 void loop() {   
-  resetCommandTimeout();
   
-  if(Serial.available()){
-    //delay(5);                              //causes trubble. sometimes it skyps some moves. change it a bit around 10 or 20 is probably good
+  
+  if(Serial.available()){                        //listening for incoming messag via usb-serial port
+    delay(5);                                    //causes trubble. sometimes it skyps some moves. change it a bit around 10 or 20 is probably good
     String incomingValue = Serial.readString();   
     
-    if(incomingValue == "A"){  //se legge 100 allora puoi eseguire le funzioni.
-      //delay(20);
-      //int str_len = bits.length();
-      //Serial.print(bits);
+    if(incomingValue == "A"){                    //once true begin main function
+
       createAndControlFunc(bits);
-      //Serial.flush(); //cancella tutto quello che che rimane nel buffer seriale
-      //bits = "";
     }
     else{
       bits += incomingValue;
@@ -106,54 +102,47 @@ void loop() {
 void(*resetFunc) (void) = 0; //reset all data  
 
 
-void createAndControlFunc(String message){ // funzione centrale  
+void createAndControlFunc(String message){ //main funuction 
 
   int len = message.length()+1;
-  int num_of_parity_digits = 1;
-  char hamming_list[len];
+  int num_of_parity_digits = 1; 
+  char hamming_list[len]; // message in to char array
   message.toCharArray(hamming_list, len);
 
-  bool control = Hamming(hamming_list, len);
+  bool control = Hamming(hamming_list, len); //controll hamming digits
   
-  while(message.length() > pow(2, num_of_parity_digits)){
+  while(message.length() > pow(2, num_of_parity_digits)){ //search amount of parity digits
     num_of_parity_digits ++;
   }
-  char removed_parity [message.length()- num_of_parity_digits - 1];
+  char removed_parity [message.length() - num_of_parity_digits - 1]; // also minus the total parity digit (0)
 
   int j = 0;
   for(int i=3; i< len; i++){
     float p = log(i)/log(2);
     if(p - (int)p != 0){
-        removed_parity[j] = hamming_list[i];
+        removed_parity[j] = hamming_list[i]; //eliminate the parity digits
         j++;
     }
   }
   int len_remove = sizeof(removed_parity);
   String moves [(int)(len_remove / 8)];
-  /*if(len_remove == 160){
-  Serial.println("111");
-  }*/
-  //Serial.print("111");
-  for(int count = 0; count < (int)(len_remove / 8); count++){ 
+
+  for(int count = 0; count < (int)(len_remove / 8); count++){ //take 8 bit chars and convert them into decimal
     String bitstring = "";
     for(int k = count * 8; k < (count + 1) * 8; k++){
       bitstring += removed_parity[k];
     }
     //Serial.println(bitstring);
-    moves[count] = binaryToDec(bitstring);
-    //Serial.println(moves[count]);
-    //delay(30);
+    moves[count] = binaryToDec(bitstring); // translate the 8 binary bit string in deimal 
+
   }
 
   if(control = true) {
-    //Serial.print("101")     
-    stepped(moves, sizeof(moves)); // controlla le rotazione che ogni motore deve eseguire passando per i valori del vettore controllato in precedenza
-    //delay(4000);
-    //Serial.print("100"); // manda un segnale a processing che determina la fine dell'esecuzione dei movimenti
+    stepped(moves, sizeof(moves)); // stepp the motors according to the moves Array
   }
   
-  Serial.flush(); //cancella tutto quello che che rimane nel buffer seriale
-  bits = "";
-  //resetFunc();    //reset di tutti i dati 
+  Serial.flush(); // clear the buffer
+  bits = "";      // null the initial string
+  //resetFunc();    
    
 }
